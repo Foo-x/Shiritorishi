b73d3fb1eced95d888b95af75fcd3eadeab0d8de
Foo-x <www.sand.x@gmail.com>
2018-12-06 23:53:54 +0900

    add: content of help modal

--- assets/elm/src/Component/HelpModal.elm
+++ assets/elm/src/Component/HelpModal.elm
@@ -29,19 +29,219 @@ view model =
             []
         , div
             [ class "modal-content" ]
-            [ div
-                [ class "card" ]
+            [ article
+                [ class "message" ]
                 [ div
-                    [ class "card-content" ]
-                    [ text "hello" ]
+                    [ class "message-header has-background-light has-text-dark" ]
+                    [ p
+                        []
+                        [ text "ルール" ]
+                    , button
+                        [ class "delete"
+                        , attribute "aria-label" "close"
+                        , onClick Inactivate
+                        ]
+                        []
+                    ]
+                , div
+                    [ id "help-content"
+                    , class "message-body has-background-white content" ]
+                    [ ul
+                        []
+                        [ li
+                            []
+                            [ text "直前の最後の文字で始める必要があります。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        []
+                                        [ text "しりと" ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "り" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "り" ]
+                                    , span
+                                        []
+                                        [ text "んご" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "最後の文字に「ん」や「ン」を使用することはできません。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        [ class "icon is-small has-text-danger" ]
+                                        [ i
+                                            [ class "fas fa-times" ]
+                                            []
+                                        ]
+                                    , span
+                                        []
+                                        [ text "ライオ" ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "ン" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "使用できる文字はひらがなとカタカナのみです。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        []
+                                        [ text "しりとり" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "icon is-small has-text-danger" ]
+                                        [ i
+                                            [ class "fas fa-times" ]
+                                            []
+                                        ]
+                                    , span
+                                        []
+                                        [ text "林檎" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "1文字の単語は使用できません。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        []
+                                        [ text "りす" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "icon is-small has-text-danger" ]
+                                        [ i
+                                            [ class "fas fa-times" ]
+                                            []
+                                        ]
+                                    , span
+                                        []
+                                        [ text "す" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "直前の50個中に使用されている単語は使用できません。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "りす" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        []
+                                        [ text "すきま" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        []
+                                        [ text "まり" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "icon is-small has-text-danger" ]
+                                        [ i
+                                            [ class "fas fa-times" ]
+                                            []
+                                        ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "りす" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "直前の最後の文字が長音(ー)の場合、その直前の文字で始める必要があります。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        []
+                                        [ text "コー" ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "ヒ" ]
+                                    , span
+                                        []
+                                        [ text "ー" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "ひ" ]
+                                    , span
+                                        []
+                                        [ text "まわり" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "直前の最後の文字が小文字の場合、大文字に直した文字で始める必要があります。"
+                            , ul
+                                []
+                                [ li
+                                    []
+                                    [ span
+                                        []
+                                        [ text "きし" ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "ゃ" ]
+                                    , span
+                                        []
+                                        [ text " → " ]
+                                    , span
+                                        [ class "has-text-weight-bold" ]
+                                        [ text "や" ]
+                                    , span
+                                        []
+                                        [ text "さい" ]
+                                    ]
+                                ]
+                            ]
+                        , li
+                            []
+                            [ text "名前や単語は20文字以下にする必要があります。" ]
+                        ]
+                    ]
                 ]
             ]
-        , button
-            [ class "modal-close is-large"
-            , attribute "aria-label" "close"
-            , onClick Inactivate
-            ]
-            []
         ]
 
 
